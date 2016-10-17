dd70c6c35d009de78db90c615e9ebd655b5f9421
Jonathan Beaulieu <123.jonathan@gmail.com>
2016-10-17 09:19:13 -0500

    Refactored package name to littlepython

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 # TODO: add better metadata https://python-packaging.readthedocs.io/en/latest/metadata.html
 
-setup(name='littlepy',
+setup(name='littlepython',
     version='0.1.1',
     description='A Super Simplified Python with a Little Syntactic Sugar',
     url='https://github.com/DerPferd/little-python',
     author='Jonathan Beaulieu',
     author_email='123.jonathan@gmail.com',
     license='MIT',
-    packages=['littlepy'],
+    packages=['littlepython'],
     zip_safe=False,
     test_suite='nose.collector',
     tests_require=['nose'],
