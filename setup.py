ba0814bae0171ee3d939c93e71edf1e91b4a73cb
Jonathan Beaulieu <123.jonathan@gmail.com>
2017-03-19 16:56:52 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 # TODO: add better metadata https://python-packaging.readthedocs.io/en/latest/metadata.html
 
 setup(name='littlepython',
-    version='0.1.4',
-    description='A Super Simplified Python with a Little Syntactic Sugar',
-    url='https://github.com/DerPferd/little-python',
-    author='Jonathan Beaulieu',
-    author_email='123.jonathan@gmail.com',
-    license='MIT',
-    packages=['littlepython'],
-    zip_safe=False,
-    test_suite='nose.collector',
-    tests_require=['nose'],
-    scripts=['bin/littlepy'])
+      version='0.1.5',
+      description='A Super Simplified Python with a Little Syntactic Sugar',
+      url='https://github.com/DerPferd/little-python',
+      author='Jonathan Beaulieu',
+      author_email='123.jonathan@gmail.com',
+      license='MIT',
+      packages=['littlepython'],
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      scripts=['bin/littlepy'])
